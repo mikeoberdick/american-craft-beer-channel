a007e17f74a84a6d971bc793813d7d4e9461b056
mikeoberdick <mike@designs4theweb.com>
2021-01-19 05:22:32 -0500

    new footer

--- footer.php
+++ footer.php
@@ -17,13 +17,13 @@ defined( 'ABSPATH' ) || exit;
 <footer>
 	<div class="container py-3">
 		<div class="row">
-			<div class="col-sm-12 d-flex justify-content-center align-items-center">
-				<div class = "text-left">
-					<h6 class = "roboto watch-now">Watch Now on</h6>
+			<div class="col-sm-12 d-flex flex-column align-items-center">
+				<div class = "footer-button">
+					<h6 class = "roboto watch-now mb-0 mr-2">Watch Now on</h6>
 					<a target = "_blank" href = "<?php the_field('roku_url', 'option'); ?>"><img src="<?php echo get_stylesheet_directory_uri() . '/img/roku.png'; ?>" alt="Roku"></a>
+				</div><!-- .watch-now -->
 				<p class = "mb-0 small mt-3">&copy <?php echo date('Y'). ' ' . get_bloginfo('name') . '. ' . 'All rights reserved.'; ?></p>
 				<p class = "mb-0 small">Website designed and developed by <a rel="noreferrer" target = "_blank" href = "https://designs4theweb.com" alt = "Designs 4 The Web WordPress Website Builders">Designs 4 The Web</a></p>
-				</div>
 			</div><!--col-md-12 -->
 		</div><!-- row -->
 	</div><!-- .container -->
